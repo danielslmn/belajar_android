818da7ce222dce984875d7026eb29d62d8d59f24
Daniel <danielysulaiman@gmail.com>
2020-09-03 22:10:11 +0700

    pertemuan1

---
+++ app/src/main/java/com/example/homework1_homepagecovidapp/Lookout.kt
@@ -0,0 +1,11 @@
+package com.example.homework1_homepagecovidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lookout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lookout)
+    }
+}
